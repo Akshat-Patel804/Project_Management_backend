4f72a7feea02967f6bcb80ce3b05652fd7259a56
aksaht patel <akshatpatel804@gmail.com>
2026-01-10 14:49:57 +0100

    updated file structure and cors

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import dotenv from 'dotenv';
+import app from './app.js';
+
+
+dotenv.config({
+    path: "./.env",
+});
+
+
+const port = process.env.PORt || 3000;
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
