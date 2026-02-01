6449ff3a6bc41b4e71a2eebefa3095aecc11ce02
aksaht patel <akshatpatel804@gmail.com>
2026-02-01 13:12:36 +0100

    feat: implement unsecured auth routes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,13 +1,18 @@
 import { Router } from "express";
-import { registerUser,login,logoutUser } from "../controllers/auth.controllers.js"
+import { registerUser,login,logoutUser, verifyEmail, refreshAccessToken } from "../controllers/auth.controllers.js"
 import { validate } from "../middlewares/validator.middlewares.js";
 import { userRegisterValidator,userLoginValidator } from "../validators/index.js";
 import {verifyJWT} from "../middlewares/auth.middlewares.js";
 
 const router = Router();
 
+// unsecured routes
 router.route("/register").post(userRegisterValidator(),validate,registerUser);
 router.route("/login").post(userLoginValidator(),validate,login);
+router.route("/verify-email/:varificationToken").get(verifyEmail);
+router.route("/refresh-token").post(refreshAccessToken);
+
+// secured routes
 router.route("/logout").post(verifyJWT,logoutUser);
 
 
